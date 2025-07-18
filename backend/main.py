b2df392a63bc9ed218b90424e575eeef232ae905
rmluck <rohandm99@yahoo.com>
2025-07-18 16:48:15 -0700

    Moved frontend deployment to Netlify.

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nfl-mock-draft-simulator-frontend.onrender.com"],
+    allow_origins=["nfl-mock-draft-simulator.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
