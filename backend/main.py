c3ab90f743f9049276b87f9ed0081815055c73dc
rmluck <rohandm99@yahoo.com>
2025-07-18 15:26:27 -0700

    Fixed bug with CORS middleware.

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="https://nfl-mock-draft-simulator-frontend.onrender.com",
+    allow_origins=["https://nfl-mock-draft-simulator-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
